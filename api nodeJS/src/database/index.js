6692789c9fc96f7b6c79a4d7024d01d1df9f3b8a
Ciirilo <69328075+ciirilo@users.noreply.github.com>
2021-04-07 02:43:55 -0300

    Upload apinodeJS

---
+++ api nodeJS/src/database/index.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose')
+
+mongoose.Promise = global.Promise
+
+mongoose.connect("mongodb://localhost/noderest", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
+
+module.exports = mongoose
+   
